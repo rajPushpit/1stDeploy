9e2f2c35cadbd1781ff91155dc42e56d8ff766f1
rajPushpit <pushpitraj19jan@gmail.com>
2024-10-04 02:27:29 +0530

    hhh

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/login',(req, res)=>{
 app.get('/youtube',(req,res)=>{
     res.send('<center>3RD</center>')
 })
+app.get('/youtub',(req,res)=>{
+    res.send('<center>RD</center>')
+})
+app.get('/youtu',(req,res)=>{
+    res.send('<center>4th</center>')
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
